f039c8fb0608ed6303ab8f6e2aa21b10b65aeeb2
Ryoji Kato <wak109@gmail.com>
2017-05-01 19:07:23 +0900

    enhanced tests

--- src/test/scala/ExcelerSuite.scala
+++ src/test/scala/ExcelerSuite.scala
@@ -1,10 +1,59 @@
-import org.scalatest.FunSuite
+/* vim: set ts=4 et sw=4 sts=4 fileencoding=utf-8: */
+import scala.util.{Try, Success, Failure}
+import org.scalatest._
 
-class ExcelerSuite extends FunSuite {
+import java.nio.file.{Paths, Files}
+
+class ExcelerSuite extends FunSuite with BeforeAndAfterEach {
   
-  test("Create Excel Workbook") {
-      val workbook = ExcelerWorkbook.create()
-      ExcelerWorkbook.createSheet(workbook, "test")
-      ExcelerWorkbook.saveAs(workbook, "hehe.xlsx")
-  }
+    val testFile = "testFile.xlsx"
+    val testSheet = "testSheet"
+    val testMessage = "Hello, world"
+
+    override def beforeEach() {
+        Files.deleteIfExists(Paths.get(testFile))
+    }
+    
+    override def afterEach() {
+        Files.deleteIfExists(Paths.get(testFile))
+    }
+    
+    test("Create Excel Workbook") {
+        val workbook = ExcelerWorkbook.create()
+        ExcelerWorkbook.saveAs(workbook, testFile)
+        assert(Files.exists(Paths.get(testFile)))
+    }
+
+    test("Create Excel Sheet") {
+        val workbook = ExcelerWorkbook.create()
+
+        ExcelerWorkbook.createSheet(workbook, testSheet) match {
+            case Success(_) => assert(true)
+            case Failure(e) => assert(false)
+        }
+
+        /* Can't create sheets of same name */
+        ExcelerWorkbook.createSheet(workbook, testSheet) match {
+            case Success(_) => assert(false)
+            case Failure(e) => assert(true)
+        }
+    }
+
+    test("Create Set Cell Value") {
+        val workbook = ExcelerWorkbook.create()
+        (
+            for { 
+                sheet <- ExcelerWorkbook.createSheet(workbook, testSheet)
+                row = sheet.createRow(0)
+                cell = row.createCell(0)
+            } yield cell
+        ) match { 
+            case Success(cell) => {
+                cell.setCellValue(testMessage)
+            }
+            case Failure(e) => println(e.getMessage())
+        } 
+        ExcelerWorkbook.saveAs(workbook, testFile)
+        assert(Files.exists(Paths.get(testFile)))
+    }
 }
