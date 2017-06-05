a676853e0a5a8f423d6f3b2efe5c5e58f468881a
Ryoji Kato <wak109@gmail.com>
2017-06-05 18:00:06 +0900

    snapshot

--- src/main/scala/Exceler.scala
+++ src/main/scala/Exceler.scala
@@ -9,12 +9,12 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.io._
 import java.nio.file._
 
+import ExcelLib._
 import ExcelRectangle._
 
 object Exceler {
 
     def convertExcelTableToXML(filename:String):Try[Unit] = {
-
         Try {
             val file = new File(filename)
             val workbook = WorkbookFactory.create(file, null ,true) 
@@ -25,6 +25,22 @@ object Exceler {
                 cell <- row.getColumnList
             } {
                 println(cell)
+            }
+        }
+    }
+
+    def readExcelTables(
+        filename:String,
+        sheetname:String,
+        tablename:String,
+        rowKeys:String,
+        colKeys:String):Try[Unit] = {
+        Try {
+            val file = new File(filename)
+            val workbook = WorkbookFactory.create(file, null ,true) 
+            workbook.getSheetOption(sheetname) match {
+                case Some(sheet) => println("Some")
+                case None => println("None")
             }
         }
     }
