f4f8eb6ae12684af4ccc9b7bf1f42031fa42c23d
wak109 <wak109@gmail.com>
2017-08-06 11:41:45 +0900

    fixed main

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -7,27 +7,27 @@ import org.scalatra.LifeCycle
 import javax.servlet.ServletContext
 
 
-object JettyLauncher extends App {
-  override def main(args: Array[String]) {
+object JettyLauncher {
+  def main(args: Array[String]) {
     val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
 
-      val server = new Server()
+    val server = new Server()
 
-      val connector = new ServerConnector(server)
-      connector.setInheritChannel(true)
-      connector.setPort(port)
+    val connector = new ServerConnector(server)
+    connector.setInheritChannel(true)
+    connector.setPort(port)
 
-      server.setConnectors(Array(connector))
+    server.setConnectors(Array(connector))
 
-      val context = new WebAppContext()
-      context.setContextPath("/")
-      context.setResourceBase("src/main/webapp")
-      context.addEventListener(new ScalatraListener)
-      context.addServlet(classOf[DefaultServlet], "/")
+    val context = new WebAppContext()
+    context.setContextPath("/")
+    context.setResourceBase("src/main/webapp")
+    context.addEventListener(new ScalatraListener)
+    context.addServlet(classOf[DefaultServlet], "/")
 
-      server.setHandler(context)
+    server.setHandler(context)
 
-      server.start
-      server.join
+    server.start
+    server.join
   }
 }
