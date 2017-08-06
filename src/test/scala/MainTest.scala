9afa00e24a69781ff6aa2112198b24e2d440f3a7
Ryoji Kato <wak109@gmail.com>
2017-08-06 18:18:49 +0900

    Added the test for Main

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,10 +1,21 @@
+/* vim: set ts=2 et sw=2 sts=2 fileencoding=utf-8: */
+import scala.util.{Try, Success, Failure}
 import org.scalatest.FunSuite
 
 class MainSuite extends FunSuite {
   
   test("Main.parseCommandLine") {
-    val cmdline = Main.parseCommandLine(Array("test.xlsx"))
-    assert(1 === 1)
+    Main.parseCommandLine(Array()) match {
+      case Success((Nil, false, 8080, ".")) => assert(true)
+      case _ => assert(false)
+    }
+    Main.parseCommandLine(Array("-p", "9999")) match {
+      case Success((Nil, false, 9999, ".")) => assert(true)
+      case _ => assert(false)
+    }
+    Main.parseCommandLine(Array("-p", "abcd")) match {
+      case Failure(_) => assert(true)
+      case _ => assert(false)
+    }
   }
-
 }
